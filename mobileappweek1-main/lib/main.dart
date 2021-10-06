20c0320f12ca399da1d63fec2e3c82d412cb5fa1
Pattaraphon <s6302041510087@email.kmutnb.ac.th>
2021-10-07 02:09:55 +0700

    Dashbord

--- mobileappweek1-main/lib/main.dart
+++ mobileappweek1-main/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Index(),
+      home: Dashboard(),
       routes: {
         'Login': (context) => Login(),
         'Register': (context) => Register(),
